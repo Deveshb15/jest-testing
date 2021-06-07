940b424e6d13d2cfbf02e7dc7ddcea9e4f046dd8
Deveshb15 <deveshb583@gmail.com>
2021-06-07 19:34:57 +0530

    First Test

--- src/script.js
+++ src/script.js
@@ -7,12 +7,14 @@ const googleDatabase = [
     'animeninja.com'
 ]
 
-const googleSearch = (searchInput) => {
-    const matches = googleDatabase.filter(website => {
+const googleSearch = (searchInput, db) => {
+    const matches = db.filter(website => {
         return website.includes(searchInput)
     })
 
     return matches.length > 3 ? matches.slice(0,3) : matches
 }
 
-console.log(googleSearch('anime'));
+// console.log(googleSearch('anime'));
+
+module.exports = googleSearch
--- src/script.test.js
+++ src/script.test.js
@@ -1,3 +1,18 @@
-it('this is a test', () => {
-    
+const googleSearch = require('./script')
+
+const dbMock = [
+    'cat.com',
+    'dog.com',
+    'doglover.com',
+    'hello.com',
+    'dogis.com'
+]
+
+it('it is a silly test', () => {
+    expect('anime').toBe('anime')
+})
+
+it('searching google', () => {
+    expect(googleSearch('nothing',dbMock)).toEqual([])
+    expect(googleSearch('dog',dbMock)).toEqual(["dog.com", "doglover.com", "dogis.com"])
 })
