1afb188ba8bbe42c8b2a4c979646740ff54c46d6
Deveshb15 <deveshb583@gmail.com>
2021-06-07 19:41:22 +0530

    First Test done

--- src/script.test.js
+++ src/script.test.js
@@ -12,7 +12,18 @@ it('it is a silly test', () => {
     expect('anime').toBe('anime')
 })
 
-it('searching google', () => {
-    expect(googleSearch('nothing',dbMock)).toEqual([])
-    expect(googleSearch('dog',dbMock)).toEqual(["dog.com", "doglover.com", "dogis.com"])
+describe('googleSearch', () => {
+    it('searching google', () => {
+        expect(googleSearch('nothing',dbMock)).toEqual([])
+        expect(googleSearch('dog',dbMock)).toEqual(['dog.com', 'doglover.com', 'dogis.com'])
+    })
+    
+    it('work with undefined and null input', () => {
+        expect(googleSearch(undefined, dbMock)).toEqual([])
+        expect(googleSearch(null, dbMock)).toEqual([])
+    })
+    
+    it('does not return more than 3 matches', () => {
+        expect(googleSearch('.com', dbMock).length).toEqual(3)
+    })
 })
